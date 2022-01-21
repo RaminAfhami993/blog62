433d110e67d19f6bd6761be4084d233b8ebb2506
RaminAfhami993 <raminafhami.ra@yahoo.com>
2022-01-21 12:05:29 +0330

    add authentication

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+const config = require('./config/config');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const apiRouter = require('./routes/api');
+
+const app = express();
+
+mongoose.connect(config.mongoURL)
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -19,8 +24,40 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
+app.use(
+  session({
+    key: "user_sid",
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 600000
+    },
+  })
+);
+
+
+
+app.use((req, res, next) => {
+  console.log('user_sid', req.cookies.user_sid);
+  console.log('sessin', req.session);
+
+  next();
+})
+
+
+app.use((req, res, next) => {
+  if (req.cookies.user_sid && !req.session.user) {
+    res.clearCookie('user_sid')
+  };
+  next()
+})
+
+
+
+
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +74,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
---
+++ routes/auth.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const app = require('../app');
+const router = express.Router();
+const User = require('../models/user') 
+
+router.get('/registerPage', sessionChecker, (req, res) => {
+    res.render('register', {msg: null})
+});
+
+router.get('/loginPage', sessionChecker, (req, res) => {
+    res.render('login', {msg: null})
+});
+
+router.post('/register', (req, res) => {
+    if (!req.body.firstName || !req.body.lastName || !req.body.username || !req.body.password) {
+        // return res.status(406).json({msg: 'Not Acceptable'});
+        return res.render('register', {msg: 'Not Acceptable'})
+    };
+
+    if (req.body.password.length > 50 || req.body.password.length < 8) {
+        // return res.status(406).json({msg: 'Not Acceptable'});
+        return res.render('register', {msg: 'Not Acceptable'});
+    };
+
+
+    User.findOne({username: req.body.username.trim()}, (err, existUser) => {
+        if (err) {
+            // return res.status(500).json({msg: "Somthing went wrong"})
+            return res.render('register', {msg: 'Somthing went wrong'})
+
+        };
+
+        if (existUser) {
+            // return res.status(406).json({msg: "username already token"})
+            return res.render('register', {msg: 'username already token'})
+
+        };
+
+        const NEW_USER = new User({
+            username: req.body.username,
+            lastName: req.body.lastName,
+            firstName: req.body.firstName,
+            password: req.body.password
+        });
+    
+    
+        NEW_USER.save((err, user) => {
+            if (err) {
+                // return res.status(500).json({msg: "Somthing went wrong"})
+                return res.render('register', {msg: 'Somthing went wrong'})
+
+            };
+    
+            res.redirect('/api/auth/dashboard')
+        });
+    })
+});
+
+
+router.post('/login', (req, res) => {
+    if (!req.body.username || !req.body.password) {
+        // return res.status(406).json({msg: 'Not Acceptable'});
+        return res.render('login', {msg: 'Not Acceptable'})
+    };
+
+
+    User.findOne({username: req.body.username, password: req.body.password}, (err, user) => {
+        if (err) {
+            return res.render('login', {msg: 'Somthing went wrong'})
+        } 
+        if (!user) {
+            return res.render('login', {msg: 'Wrong username or password'})
+        }
+
+        req.session.user = user;
+
+        res.redirect('/api/auth/dashboard')
+    })
+});
+
+
+
+router.get('/dashboard', (req, res) => {
+    if (!req.session.user || !req.cookies.user_sid) {
+        return res.redirect('/api/auth/loginPage')
+    };
+    res.render('dashboard', {_id: req.session.user._id, username: req.session.user.username})
+});
+
+router.get('/logout', (req, res) => {
+    res.clearCookie('user_sid');
+    res.redirect('/api/auth/loginPage')
+})
+
+
+
+
+function sessionChecker(req, res, next) {
+    if (req.session.user && req.cookies.user_sid) {
+        return res.redirect('/api/auth/dashboard')
+    } 
+    next()
+}
+
+module.exports = router;
